c9c31989882bf0d1b68452faafd34d66ae2f1598
Matthieu_Dau <matthieu@blanknetwork.com>
2022-06-16 12:56:27 +0800

    fix prettier format script + format all files

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    dirs: ['pages', 'public', 'styles'],
-  },
+	reactStrictMode: true,
+	eslint: {
+		dirs: ['pages', 'public', 'styles'],
+	},
 };
 
 module.exports = nextConfig;
